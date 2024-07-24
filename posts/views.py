9438c98b273869610c932c80624aa855e7b38796
Tomisin <fadahunsitomisin2017@gmail.com>
2024-07-24 17:17:18 +0100

    initial commit

---
+++ posts/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render, get_object_or_404
+from rest_framework import viewsets, status
+from rest_framework.request import Request
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from .models import Post
+from posts.serializers import PostSerializer
+
+
+class PostViewset(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [IsAuthenticated]
